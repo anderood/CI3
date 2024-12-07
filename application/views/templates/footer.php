c98b7714528c8f171539aac97fe0716403f8b241
Anderson <anderood@hotmail.com>
2024-12-07 19:14:22 -0300

    conclusão do primeiro tutorial

---
+++ application/views/templates/footer.php
@@ -0,0 +1,4 @@
+<em>&copy; 2015</em>
+<p>Este é o footer</p>
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+<body>
+
+<h2>ESte é o Header</h2>
+<h1><?php echo $title; ?></h1>
